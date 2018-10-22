1c448f6b0495a301056dad39176c662047e27d75
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2018-10-22 23:30:14 +0100

    Compelted redesigned landing page, added js parrallax scrolling background, embedded preview video, download links, screenshots and press pack download linkl

--- OddityWebsiteLanding/wwwroot/js/site.js
+++ OddityWebsiteLanding/wwwroot/js/site.js
@@ -1,1 +1,60 @@
-﻿// Write your Javascript code.
+﻿
+
+//Side Bar Menu Code
+$(document).ready(function () {
+    $(document).ready(function () {
+        // HTML markup implementation, overlap mode, initilaize collapsed
+        $('#menu').multilevelpushmenu({
+            containersToPush: [$('#pushobj')],
+            collapsed: true
+        });
+
+        $('#myLink').click(function () { MyFunction(); return false; });
+
+        //setInterval(AnimateItems, 15);
+    });
+});
+
+$(window).resize(function () {
+
+    if ($(this).width() < 1024) {
+
+        $('#menu').hide();
+
+    } else {
+
+        $('#menu').show();
+
+    }
+
+});
+
+$("a[href='#top']").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+});
+
+$("a[href='#trailer']").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#trailer").offset().top
+    }, 2000);
+});
+
+$("a[href='#download']").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#StoreButtonsContent").offset().top
+    }, 2000);
+});
+
+$("a[href='#screenshots']").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#ScreenshotContainer").offset().top
+    }, 2000);
+});
+
+$("a[href='#presskit']").click(function () {
+        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+        return false;
+});
+
+
